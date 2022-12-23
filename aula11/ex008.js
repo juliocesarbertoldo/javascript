b66d4b8cac619d0670724dedc77dcc5d19a3e9e5
JulioCesarBertoldo <112409550+JulioCesarBertoldo@users.noreply.github.com>
2022-12-23 16:56:01 -0300

    1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let velocidade = 60
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > 60) {
+    console.log(`Você está acima da velocidade! MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
